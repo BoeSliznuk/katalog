67f5cbecd052f351395046b966eb4b8ea002019a
Дмитрий Кобец <dima__kobets@mail.ru>
2024-07-19 04:58:37 +1000

    Поиск

--- katalog/wwwroot/js/site.js
+++ katalog/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const search = document.querySelector('[name="search"]');
+
+search.addEventListener('keyup', (e) => {
+    if (e.code === 'Enter') {
+
+        const url = '/?search=' + document.getElementById("searchinput").value;;
+
+        window.location.href = url;
+    }
+});
